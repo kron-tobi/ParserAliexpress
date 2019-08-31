807d74f215e53de390d09f3b2a67bea3dd52bec7
Vladislav <krontobi1@yandex.ru>
2019-08-31 12:48:31 +0300

    parser with 1 while()

--- src/main/java/com/krontobi/Parser.java
+++ src/main/java/com/krontobi/Parser.java
@@ -6,35 +6,27 @@ import java.util.Properties;
 
 public class Parser {
     public void doParse(BufferedReader bufferedReader) throws IOException {
-        String[] str = new String[3];
+        String[] str = new String[4];
 
         Properties parseString = new PropertyReader().getProperties();
-        System.out.println(parseString);
-        while ((str[0] = bufferedReader.readLine()) != null) {
-            if (str[0].contains(parseString.getProperty("price"))) {
-
-                str[0] = str[0].substring(0, str[0].indexOf(" руб"));
-                str[0] = str[0].replaceAll(parseString.getProperty("price"), "");
-                System.out.println(str);
-                break;
+        //System.out.println(parseString);
+        while ((str[1] = bufferedReader.readLine()) != null) {
+            if (str[1].contains(parseString.getProperty("price"))) {
+                str[1] = str[1].substring(1, str[1].indexOf(" руб"));
+                str[1] = str[1].replaceAll(parseString.getProperty("price"), "");
+                System.out.println("Цена: " + str[1]);
             }
-        }
+            else if (str[1].contains(parseString.getProperty("reviews"))) {
+                str[2] = str[1];
+                str[3] = str[2];
 
-        while ((str = bufferedReader.readLine()) != null) {
-            if (str.contains(parseString.getProperty("reviews"))) {
-                str = str.substring(str.indexOf("\"totalValidNum\":"), str.indexOf(",\"trialReviewNum\":0,"));
-                str = str.replaceAll(parseString.getProperty("reviews"), "");
-                System.out.println(str);
-                break;
-            }
-        }
+                str[2] = str[2].substring(str[1].indexOf("\"totalValidNum\":"), str[1].indexOf(",\"trialReviewNum\":0,"));
+                str[2] = str[2].replaceAll(parseString.getProperty("reviews"), "");
+                System.out.println("Отзывы: " + str[1]);
 
-        while ((str = bufferedReader.readLine()) != null) {
-            if (str.contains(parseString.getProperty("orders"))) {
-                str = str.substring(str.indexOf(parseString.getProperty("orders")), str.indexOf(",\"tradeCountUnit\""));
-                str = str.replaceAll(parseString.getProperty("orders"), "");
-                System.out.println(str);
-                break;
+                str[3] = str[3].substring(str[2].indexOf(parseString.getProperty("orders")), str[2].indexOf(",\"tradeCountUnit\""));
+                str[3] = str[3].replaceAll(parseString.getProperty("orders"), "");
+                System.out.println("Заказ(ов): " + str[2]);
             }
         }
     }
